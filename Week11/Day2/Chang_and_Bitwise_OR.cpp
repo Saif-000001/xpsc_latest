80a55479d5d27279338d3b3e955c11d9f5305284
MSI75 <sisaif56@gmail.com>
2023-08-19 00:28:27 +0600

    Chang and Bitwise OR

---
+++ Week11/Day2/Chang_and_Bitwise_OR.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    ll ans = a[0];
+    for (int i = 0; i < n; i++)
+        ans |= a[i];
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
