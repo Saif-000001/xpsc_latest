aea6fc02d3d1961350cffe09d0793a6e3fcb757f
MSI75 <sisaif56@gmail.com>
2023-08-19 03:04:19 +0600

    AGAGA XOOORRR

---
+++ Week11/Day2/AGAGA_XOOORRR.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int x = 0;
+    for (int i = 0; i < n; i++)
+        x ^= a[i];
+    if (x == 0)
+        cout << "YES\n";
+    else
+    {
+        int k = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            k ^= a[i];
+            if (k == x)
+            {
+                k = 0;
+                cnt++;
+            }
+        }
+        // cout<<cnt<<"\n";
+        if (cnt > 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
