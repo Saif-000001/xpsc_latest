59117c0f167b8b457b96b76d64d2907881e52977
MSI75 <sisaif56@gmail.com>
2023-08-22 12:47:38 +0600

    Morning Sandwich

---
+++ Week11/Day5/Morning_Sandwich.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int b, c, h;
+    cin >> b >> c >> h;
+    cout << min(b-1, c+h) * 2 + 1 << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
