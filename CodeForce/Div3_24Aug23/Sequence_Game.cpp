633e967b14db6ad43ab2100abf882ed84e9fdde4
MSI75 <sisaif56@gmail.com>
2023-08-25 18:15:39 +0600

    Sequence Game

---
+++ CodeForce/Div3_24Aug23/Sequence_Game.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<int> ans;
+    ans.push_back(a[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i - 1] <= a[i])
+            ans.push_back(a[i]);
+        else
+        {
+            ans.push_back(1);
+            ans.push_back(a[i]);
+        }
+    }
+
+    cout << ans.size() << "\n";
+    for (auto i : ans)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
