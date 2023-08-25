2c4d6b81ef9c3f78e89771e210879fd77eb3732a
MSI75 <sisaif56@gmail.com>
2023-08-25 23:17:27 +0600

    k th divisor

---
+++ Week12/Day1/k_th_divisor.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> ans;
+    for (ll i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            ans.push_back(i);
+            if ((n / i) != i)
+                ans.push_back(n / i);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    if (ans.size() < k)
+        cout << -1 << "\n";
+    else
+        cout << ans[k - 1] << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
