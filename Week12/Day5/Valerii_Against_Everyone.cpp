7738fb76ac4a2603e196a57d596283bae069d7bc
MSI75 <sisaif56@gmail.com>
2023-08-29 19:12:35 +0600

    Valerii Against Everyone

---
+++ Week12/Day5/Valerii_Against_Everyone.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+
+    if (st.size() != n)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
