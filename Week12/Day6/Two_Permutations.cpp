c7342ee49adca8a2c5fdd2ae37408074095ab83c
MSI75 <sisaif56@gmail.com>
2023-08-30 10:59:19 +0600

    Two Permutations

---
+++ Week12/Day6/Two_Permutations.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    if (n == a && a == b)
+        cout << "YES\n";
+    else if (a + b < n - 1)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
