e79f61875e62135d5b24f317cdf411d1303633c8
MSI75 <sisaif56@gmail.com>
2023-08-31 17:01:45 +0600

    Relatively Prime Pairs

---
+++ Week12/Day7/Relatively_Prime_Pairs.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll l, r;
+    cin >> l >> r;
+    if (l > r)
+    {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+    while (l < r)
+    {
+        cout << l << " " << l + 1 << "\n";
+        l += 2;
+    }
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
