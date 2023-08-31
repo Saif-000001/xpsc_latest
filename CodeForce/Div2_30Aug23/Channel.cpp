dcf7f7ccbd4615fd739b35c9845a0cdd241e3555
MSI75 <sisaif56@gmail.com>
2023-08-31 13:48:35 +0600

    I HATE IN MY LIFE

---
+++ CodeForce/Div2_30Aug23/Channel.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, a, q;
+    cin >> n >> a >> q;
+
+    string s;
+    cin >> s;
+
+    a = a - 1;
+    if (n < a + q)
+        cout << "YES\n";
+    else if (n > a + q)
+        cout << "NO\n";
+    else
+        cout << "MAYBE\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
