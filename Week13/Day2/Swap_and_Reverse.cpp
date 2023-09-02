00e8d185f442318a78a4f06e9abd043db9b09106
MSI75 <sisaif56@gmail.com>
2023-09-02 14:05:22 +0600

    Swap and Reverse

---
+++ Week13/Day2/Swap_and_Reverse.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    if ((k & 1) == 0)
+    {
+        sort(s.begin(), s.end());
+        cout << s << "\n";
+        return;
+    }
+
+    string a, b;
+    for (int i = 0; i < n; i++)
+    {
+        if (i & 1)
+            a += s[i];
+        else
+            b += s[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int l = 0, r = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i & 1)
+            s[i] = a[l++];
+        else
+            s[i] = b[r++];
+    }
+
+    cout << s << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
