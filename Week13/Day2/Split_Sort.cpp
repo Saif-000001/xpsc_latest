e237e3375ed4adef5ca5c376321149aeefd1363c
MSI75 <sisaif56@gmail.com>
2023-09-02 14:07:24 +0600

    Split Sort

---
+++ Week13/Day2/Split_Sort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n;
+    cin >> n;
+    vector<pii> v(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i].first;
+        v[v[i].first].second = i;
+    }
+
+    int ans = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (v[i - 1].second > v[i].second)
+            ans++;
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
