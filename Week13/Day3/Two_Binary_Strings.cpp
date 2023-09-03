4f1cab3b2a37659a658b1fe68aaa74a015f66c02
MSI75 <sisaif56@gmail.com>
2023-09-03 17:27:48 +0600

    Two Binary Strings

---
+++ Week13/Day3/Two_Binary_Strings.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    string s, t;
+    cin >> s >> t;
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     if (s[i] == '0' && s[i + 1] == '1')
+    //     {
+    //         if (t[i] == '0' && t[i + 1] == '1')
+    //         {
+    //             cout << "YES\n";
+    //             return;
+    //         }
+    //     }
+    // }
+    // cout << "NO\n";
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == t[i] && s[i + 1] == t[i + 1] && s[i] == '0' && t[i + 1] == '1')
+        {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
