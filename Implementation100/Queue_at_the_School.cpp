3c0d9ef577bb1cc7df736cb7a97d09c5098b923d
MSI75 <sisaif56@gmail.com>
2023-09-08 10:55:12 +0600

    Queue at the school

---
+++ Implementation100/Queue_at_the_School.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, t;
+    cin >> n >> t;
+
+    string s;
+    cin >> s;
+
+    while (t--)
+    {
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'B' && s[i + 1] == 'G')
+            {
+                swap(s[i], s[i + 1]);
+                i++;
+            }
+        }
+    }
+    cout << s << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
