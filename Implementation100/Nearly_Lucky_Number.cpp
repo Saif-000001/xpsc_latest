2068a9f66f6e7ee12846b8306419f8c19eb93e16
MSI75 <sisaif56@gmail.com>
2023-09-08 22:34:50 +0600

    Nearly Lucky Number

---
+++ Implementation100/Nearly_Lucky_Number.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    while (n)
+    {
+        int digit = n % 10;
+        if (digit == 4 || digit == 7)
+            ans++;
+        n /= 10;
+    }
+
+    if (ans == 7 || ans == 4)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
