fca16a92d436f6565d437f806880a6afc73f70e0
MSI75 <sisaif56@gmail.com>
2023-09-08 20:09:26 +0600

    Word

---
+++ Implementation100/Word.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    string s;
+    cin >> s;
+
+    int u = 0;
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] < 96)
+            u++;
+    int n = s.size() - u;
+
+    if (u > n)
+    {
+        for (int i = 0; i < s.size(); i++)
+            if (s[i] > 96)
+                s[i] -= 32;
+        cout << s << "\n";
+    }
+    else
+    {
+        for (int i = 0; i < s.size(); i++)
+            if (s[i] < 96)
+                s[i] += 32;
+        cout << s << "\n";
+    }
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
