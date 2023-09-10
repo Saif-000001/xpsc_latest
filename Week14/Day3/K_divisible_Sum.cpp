b18d88752af45a325b9ba52e39bc94cd28916740
MSI75 <sisaif56@gmail.com>
2023-09-10 15:38:17 +0600

    K_divisible sum

---
+++ Week14/Day3/K_divisible_Sum.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int ans = n / k * k;
+    if (ans < n)
+        ans += k;
+
+    cout << (ans - 1) / n + 1 << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
