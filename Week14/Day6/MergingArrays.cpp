0a4b637a59c08d9d6beb3c133175765fbf6620d0
MSI75 <sisaif56@gmail.com>
2023-09-13 17:39:08 +0600

    MergingArrays

---
+++ Week14/Day6/MergingArrays.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    vector<ll> ans(n + m);
+    int i = 0, j = 0, k = 0;
+    while (i < n || j < m)
+    {
+        if (j == m || i < n && a[i] < b[j])
+        {
+            ans[k] = a[i];
+            i++, k++;
+        }
+        else
+        {
+            ans[k] = b[j];
+            j++, k++;
+        }
+    }
+
+    for (auto i : ans)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
