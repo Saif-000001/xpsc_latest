94d9df23a443957e996d2727a476a8a2d38b73a6
MSI75 <sisaif56@gmail.com>
2023-09-14 15:55:46 +0600

    Apple Division

---
+++ Week14/Day7/Apple_Division.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    ll ans = INT_MAX;
+    for (int mask = 0; mask < 1 << n; mask++)
+    {
+        ll tumb = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (mask & 1 << i)
+                tumb += a[i];
+        }
+
+        if (abs(sum - 2 * tumb) < ans)
+            ans = abs(sum - 2 * tumb);
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
