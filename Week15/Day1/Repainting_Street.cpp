460ebed1979abfc473a860f195a6bf70414050d3
MSI75 <sisaif56@gmail.com>
2023-09-15 16:41:04 +0600

    Repainting Street

---
+++ Week15/Day1/Repainting_Street.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = INT_MAX;
+    for (int i = 0; i < 101; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n;)
+        {
+            if (a[j] == i)
+                j++;
+            else
+            {
+                cnt++;
+                j += k;
+            }
+        }
+        ans = min(ans, cnt);
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
