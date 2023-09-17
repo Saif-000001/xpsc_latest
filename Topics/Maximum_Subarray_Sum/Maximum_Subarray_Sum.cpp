50bf35361d62d2921d961abf9e8e9621c21287f2
MSI75 <sisaif56@gmail.com>
2023-09-18 01:02:45 +0600

    Maximum Subarray sum

---
+++ Topics/Maximum_Subarray_Sum/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    ll sum = 0, ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        sum = max(a[i], sum + a[i]);
+        ans = max(ans, sum);
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
