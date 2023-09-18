56cd537a897abed78d9a16469a8a2bc6f2175ca1
MSI75 <sisaif56@gmail.com>
2023-09-18 23:29:13 +0600

    Playlist

---
+++ Week15/Day4/Playlist.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    ll start = 0;
+    set<ll> s;
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (s.count(a[i]))
+        {
+            s.erase(a[start]);
+            start++;
+        }
+        s.insert(a[i]);
+        ans = max(ans, (ll)s.size());
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
