d71ce7bc071fb443f16f63ac5d08ef17d3579bfe
MSI75 <sisaif56@gmail.com>
2023-09-20 11:20:16 +0600

    Creating Strings

---
+++ Week15/Day6/Creating_Strings.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    string s;
+    cin >> s;
+    vector<string> permutation;
+    sort(s.begin(), s.end());
+    do
+    {
+        permutation.push_back(s);
+    } while (next_permutation(s.begin(), s.end()));
+
+    cout << permutation.size() << "\n";
+    for (auto i : permutation)
+        cout << i << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
