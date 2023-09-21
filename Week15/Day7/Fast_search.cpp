553dd18b4e0d88834f1139eceaf9da65f91808ff
MSI75 <sisaif56@gmail.com>
2023-09-21 16:00:33 +0600

    Fast Search

---
+++ Week15/Day7/Fast_search.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n, k, l, r;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    cin >> k;
+    vector<ll> ans;
+    while (k--)
+    {
+        cin >> l >> r;
+        ll cnt = 0;
+        auto l_b = lower_bound(a.begin(), a.end(), l);
+        auto u_b = upper_bound(a.begin(), a.end(), r);
+        cout << u_b - l_b << " ";
+    }
+    cout << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
