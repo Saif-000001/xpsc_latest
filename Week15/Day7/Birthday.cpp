bfc55dfadeb26821327964c29b4ac99a7936e681
MSI75 <sisaif56@gmail.com>
2023-09-21 16:26:56 +0600

    Birthday

---
+++ Week15/Day7/Birthday.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    vector<ll> ans(n);
+    int k = n - 1, j = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i & 1)
+            ans[j++] = a[i];
+        else
+            ans[k--] = a[i];
+    }
+
+    for (auto i : ans)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
