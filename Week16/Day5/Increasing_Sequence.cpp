ae1b5856833a397e94d20463f8a6490744c8b5e3
MSI75 <sisaif56@gmail.com>
2023-09-26 14:38:40 +0600

    Increasing Sequence

---
+++ Week16/Day5/Increasing_Sequence.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    ll ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == ans)
+            ans++;
+        ans++;
+    }
+    cout << ans - 1 << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
