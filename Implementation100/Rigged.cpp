b3ff301af9aa47004cfbad068ca5e91f1e636306
MSI75 <sisaif56@gmail.com>
2023-09-27 16:51:14 +0600

    Rigged

---
+++ Implementation100/Rigged.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n;
+    cin >> n;
+    vector<int> s(n), e(n);
+    for (int i = 0; i < n; i++)
+        cin >> s[i] >> e[i];
+
+    bool f = true;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] >= s[0] && e[i] >= e[0])
+            f = false;
+    }
+
+    if (f)
+        cout << s[0] << "\n";
+    else
+        cout << -1 << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
