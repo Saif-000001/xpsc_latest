f8fb2967b36fa7389846ac4b5e75a02693168b66
MSI75 <sisaif56@gmail.com>
2023-10-02 20:15:44 +0600

    Jellyfish and undertale

---
+++ Implementation100/Jellyfish_and_Undertale.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll a, b, n;
+    cin >> a >> b >> n;
+
+    ll ans = b;
+    for (int i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ans += min(a - 1, x);
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
