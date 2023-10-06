2a1ee19416c69cd41c40a2b16cd0c243fe661361
MSI75 <sisaif56@gmail.com>
2023-10-06 21:19:48 +0600

    Magnets

---
+++ TLE/STL/Magnets.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n;
+    cin >> n;
+    vector<string> a;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        a.push_back(s);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == a[i + 1])
+            continue;
+        else
+            ans++;
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
