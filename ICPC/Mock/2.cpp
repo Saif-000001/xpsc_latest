a45794aaad655df56ec780977cc6365cc7ec9e71
MSI75 <sisaif56@gmail.com>
2023-10-12 20:33:56 +0600

    ICPC MOCK

---
+++ ICPC/Mock/2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int x = v[i] + v[j];
+            if (a <= x && x <= b)
+                ans++;
+        }
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    int x = 1;
+    while (t--)
+    {
+        cout << "case " << x << ": ";
+        TEST_CASES();
+        x++;
+    }
+    return 0;
+}
