294f43c54ae8cb4936505b00900afe40a1f1bfa8
MSI75 <sisaif56@gmail.com>
2023-10-13 20:59:26 +0600

    Vika and Her Friends

---
+++ Implementation100/Vika_and_Her_Friends.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    int x, y;
+    cin >> x >> y;
+    string ans = "YES\n";
+    for (int i = 0; i < k; ++i)
+    {
+        int xx, yy;
+        cin >> xx >> yy;
+        if ((x + y) % 2 == (xx + yy) % 2)
+        {
+            ans = "NO\n";
+        }
+    }
+    cout << ans;
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
